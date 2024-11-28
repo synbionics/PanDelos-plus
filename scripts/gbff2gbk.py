7b5ad637933c43c6e97b778a42199a6281e09008
SimoneColli <s.colli.dev@gmail.com>
2024-11-28 15:57:40 +0100

    aggiunta Quick Start User Guide e script vari

---
+++ scripts/gbff2gbk.py
@@ -0,0 +1,18 @@
+import sys
+from os import listdir
+from os.path import isfile, join
+import re
+import shutil
+
+
+idir = sys.argv[1]
+odir = sys.argv[2]
+
+gbffFiles = [f for f in listdir(idir) if isfile(join(idir, f)) and re.match(r'^.+\.gbff$', f)]
+print(gbffFiles)
+
+for file in gbffFiles:
+    input_file_path = join(idir, file)
+    new_file_name = re.sub(r'\.gbff$', '.gbk', file)
+    output_file_path = join(odir, new_file_name)
+    shutil.move(input_file_path, output_file_path)
