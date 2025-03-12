b86f13328da4f264e0ac58a2fe627ccbe6774dc9
Simone Colli <s.colli.dev@gmail.com>
2025-03-12 21:56:06 +0100

    Listed steps for diffusivity computation

---
+++ scripts/diffusivity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+DEBUG = False
+
+
+def main():
+    # 1. determine the diffussivitty of each gene families, so the number of genomes in which it is present
+    # 2. group family by diffusivity, so crate a histogram with on x axis number in range 1 to |genomes|,
+    # the y axis number of families that appear in that number of genomes
+    # example: 10 families with diffusivity 1, 100 families with diffusivity 2, the plot will
+    # have the y value for diffusivity 1 at 10 and the y value for diffusivity 2 at 100
+    # 3. plot the histogram
+    pass
+
+if __name__ == "__main__":
+    main()
+
