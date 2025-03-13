f75e43af23629692f24be8a10a3e0c9ba9e0121f
SimoneColli <s.colli.dev@gmail.com>
2025-03-13 15:36:42 +0100

    introduction of some plots

--- scripts/diffusivity.py
+++ scripts/diffusivity.py
@@ -1,15 +1,117 @@
 #!/usr/bin/python3
 DEBUG = False
 
+import sys
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+plt.style.use('ggplot')
+
+from query_dataset.compute_families import compute_genome_families
+
+debug_families_diffusivity_output_file = "debug_families_diffusivity.json"
+debug_diffusivity_output_file = "debug_diffusivity.json"
+
+def compute_diffusivity(families, number_of_genomes):
+    fam_diffusivity = dict()
+    diffusivity = dict()
+    for i in range(number_of_genomes + 1):
+        diffusivity[i] = {
+            "number_of_families": 0,
+            "families": list()
+        }
+    
+    # print(type(families))
+    for family_id, families in families.items():
+        d = len(families["genomes"])
+        fam_diffusivity[family_id] = d
+        diffusivity[d]["number_of_families"] += 1
+        diffusivity[d]["families"].append(family_id)
+
+    if diffusivity[0]["number_of_families"] == 0:
+        del diffusivity[0]
+    
+    return [fam_diffusivity, diffusivity]
+
+def pie(diffs, ofolder, plot_type="png"):
+    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+    
+    note = list()
+    data = list()
+    for dif, content in diffs.items():
+        note.append(f"{dif} genomes")
+        data.append(content["number_of_families"])
+    
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+            bbox=bbox_props, zorder=0, va="center")
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = f"angle,angleA=0,angleB={ang}"
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(note[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
+
+    ax.set_title("Family diffusivity")
+    plt.savefig(ofolder + "pie_family_diffusivity." + plot_type)
+
+def hist(diffs, ofolder, plot_type="png"):
+    x = list()
+    y = list()
+    for dif, data in diffs.items():
+        x.append(dif)
+        y.append(data["number_of_families"])
+
+
+    plt.bar(x, y)
+    plt.xlabel("Number of genomes")
+    plt.ylabel("Number of gene families")
+    plt.savefig(ofolder + "hist_family_diffusivity." + plot_type)
+    
 
 def main():
-    # 1. determine the diffussivitty of each gene families, so the number of genomes in which it is present
+
+    # ???? Keep the number of genomes as an argument ????
+    # If i keep i can use to create a pie chart to show the percentage of
+    # genomes that have a certain number of families -> can be known as genome coverage
+    
+    if len(sys.argv) < 4:
+        print(f"Usage: python3 {sys.argv[0]} <number_of_genomes> <path_to_input_file>.clus <path_to_output_folder>")
+        exit(1)
+    
+    number_of_genomes = int(sys.argv[1])
+    ifile = sys.argv[2]
+    ofolder = sys.argv[3]
+    if ofolder[-1] != "/":
+        ofolder += "/"
+    
+    genomes, families = compute_genome_families(ifile, DEBUG)
+
+    
+    # 1. determine the diffussivity of each gene families, so the number of genomes in which it is present
     # 2. group family by diffusivity, so crate a histogram with on x axis number in range 1 to |genomes|,
     # the y axis number of families that appear in that number of genomes
     # example: 10 families with diffusivity 1, 100 families with diffusivity 2, the plot will
     # have the y value for diffusivity 1 at 10 and the y value for diffusivity 2 at 100
-    # 3. plot the histogram
-    pass
+    fam_dif, dif =  compute_diffusivity(families, number_of_genomes)
+    
+    if DEBUG:
+        with open(ofolder + debug_families_diffusivity_output_file, "w") as f:
+            json.dump(fam_dif, f, indent=4)
+            
+        with open(ofolder + debug_diffusivity_output_file, "w") as f:
+            json.dump(dif, f, indent=4)
+    
+    # 3. plot the histogram of how many families are present in how many genomes
+    hist(dif, ofolder, "pdf")
+    pie(dif, ofolder, "pdf")
+    
 
 if __name__ == "__main__":
     main()
--- scripts/pres_abs_matrix.py
+++ scripts/pres_abs_matrix.py
@@ -13,95 +13,14 @@
 import sys
 import csv
 import json
+from query_dataset.compute_families import compute_genome_families
+from query_dataset.presence_absence_matrix import compute_matrix
 
 
-DEBUG = False
+DEBUG = True
 debug_families_output_file = "output/debug_families.json"
 debug_matrix_output_file = "output/debug_matrix.csv"
 
-def compute_genome_families(ifile):
-    """
-    @brief Parses the input file to identify all genomes and gene families
-    
-    @details Reads the input clustering file and extracts information about
-             gene families and their distribution across genomes. Each line
-             in the input file represents a gene family, with the first gene
-             serving as the family identifier.
-    
-    @param ifile: Path to the input clustering file
-    @type ifile: str
-    
-    @return: A tuple containing:
-             - List of all unique genome identifiers
-             - Dictionary of family information with gene lists and genome distributions
-    @rtype: tuple(list, dict)
-    """
-    
-    genomes = set()
-    # the first gene related to the family is the represent of the family
-    families = dict()
-    
-    with open(ifile, "r") as f:
-        reader = csv.reader(f, delimiter=" ")
-        for line in reader:
-            
-            non_empty_columns = [col.strip() for col in line if col.strip() != ""]
-            if len(non_empty_columns) == 0:
-                continue
-            
-            fam_identifier = non_empty_columns[0]
-            families[fam_identifier] = {
-                "genes" : non_empty_columns,
-                "genomes": set()
-            }
-            
-            for gene in non_empty_columns:
-                genome = gene.split(":")[0]
-                families[fam_identifier]["genomes"].add(genome)
-                genomes.add(genome)
-                
-            if DEBUG:
-                print(f"fam_identifier: {fam_identifier}")
-                print(f"genes: {non_empty_columns}")
-                print(f"genomes: {families[fam_identifier]['genomes']}")
-                print(f"genomes: {genomes}")
-                print()
-                families[fam_identifier]["genomes"] = list(families[fam_identifier]["genomes"])
-    
-    genomes = list(genomes)
-    
-    return genomes, families
-
-def compute_matrix(genomes, families):
-    """
-    @brief Creates a binary presence/absence matrix for gene families across genomes
-    
-    @details Generates a 2D matrix where each row represents a gene family and
-             each column represents a genome. A value of 1 indicates the presence
-             of the genome in the family, and 0 indicates absence.
-    
-    @param genomes: List of genome identifiers
-    @type genomes: list
-    @param families: Dictionary of family information
-    @type families: dict
-    
-    @return: 2D list representing the presence/absence matrix
-    @rtype: list(list(int))
-    """
-    
-    if DEBUG:
-        for family in families:
-            families[family]["genomes"] = set(families[family]["genomes"])
-
-    # create the matrix with len(families.keys()) as row number and len (genomes) as column number
-    matrix = [[0 for _ in range(len(genomes))] for _ in range(len(families.keys()))]
-    
-    # fill the matrix
-    for i, genome in enumerate(genomes):
-        for j, family in enumerate(families.keys()):
-            if genome in families[family]["genomes"]:
-                matrix[j][i] = 1
-    return matrix
 
 def save_matrix(genomes, families, matrix, ofile):
     """
@@ -150,14 +69,14 @@ def main():
     ofile = sys.argv[2]
     
     # check input file is a .clus file
-    genomes, families = compute_genome_families(ifile)
+    genomes, families = compute_genome_families(ifile, DEBUG)
     
     if DEBUG:
         print(f"founded genomes: {genomes}")
         with open(debug_families_output_file, "w") as f:
             json.dump(families, f, indent=4)
         
-    matrix = compute_matrix(genomes, families)
+    matrix = compute_matrix(genomes, families, DEBUG)
     
     if DEBUG:
         with open(debug_matrix_output_file, "w") as f:
