0d72dfcdc3ae7006c015e95487fdc92e18c0b7c1
Pietro0392 <pontremolipietro18@gmail.com>
2025-04-02 17:57:33 +0200

    Aggiunte interfaccia e implementazione del grafo

---
+++ lib/Graph.cc
@@ -0,0 +1,37 @@
+#include "Graph.h"
+#include <iostream>
+
+void Graph::addNode(int u){
+    nodes.insert(u);
+}
+
+// non controlla esistenza effettiva dei nodi
+void Graph::addEdge(int u, int v, double w) {
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+    ++number_of_edges;
+}
+
+void Graph::printGraph() const {
+    for (const auto& [node, neighbors] : adj) {
+        std::cout << node << " -> ";
+        for (const auto& [neighbor, weight] : neighbors) {
+            std::cout << "(" << neighbor << ", " << weight << ") ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Graph::find_node(int u) const{
+    if(nodes.find(u) == nodes.end())
+        return false;
+    else return true;
+}
+
+int Graph::get_number_of_nodes() const{
+    return nodes.size();
+}
+
+int Graph::get_number_of_edges() const{
+    return number_of_edges;;
+}
---
+++ lib/Graph.h
@@ -0,0 +1,34 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+/*
+Struttura dati grafo non orientato basato su lista di adiacenza
+*/
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <utility>
+
+class Graph {
+public:
+    
+    void addNode(int u);
+
+    void addEdge(int u, int v, double w);
+
+    void printGraph() const;
+
+    bool find_node(int u) const;
+
+    int get_number_of_nodes() const;
+
+    int get_number_of_edges() const;
+
+private:
+    std::unordered_set<int> nodes;
+    std::unordered_map<int, std::vector<std::pair<int, double>>> adj;
+    int number_of_edges = 0;
+};
+
+#endif
